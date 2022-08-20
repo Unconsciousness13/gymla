dfc1adbca14252293b4a7970d7a8f17c3f533866
Pako <pakounicredit@gmail.com>
2022-08-20 14:41:38 +0200

    adding urls

--- ptf_pj/urls.py
+++ ptf_pj/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('ptf_api.urls', namespace='ptf_api')),
+    path('', include('ptf.urls'), namespace='ptf'),
 ]
