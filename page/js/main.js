209ad70ea867697650183d56ae6bd15476d53e15
Pako <pakounicredit@gmail.com>
2022-08-26 21:07:20 +0200

    Almost ready few bugs

---
+++ page/js/main.js
@@ -0,0 +1,92 @@
+'use strict';
+
+(function($) {
+
+    /*------------------
+        Preloader
+    --------------------*/
+    $(window).on('load', function() {
+        $(".loader").fadeOut();
+        $("#preloder").delay(200).fadeOut("slow");
+    });
+
+    /*------------------
+        Background Set
+    --------------------*/
+    // $('.set-bg').each(function() {
+    //     var bg = $(this).data('setbg');
+    //     $(this).css('background-image', 'url(' + bg + ')');
+
+    // });
+
+    /*------------------
+		Navigation
+	--------------------*/
+    $(".mobile-menu").slicknav({
+        prependTo: '#mobile-menu-wrap',
+        allowParentLinks: true
+    });
+
+    /*--------------------------
+        Event Slider
+    ----------------------------*/
+    $(".event__slider").owlCarousel({
+        loop: true,
+        margin: 0,
+        items: 3,
+        dots: false,
+        nav: true,
+        navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
+        smartSpeed: 1200,
+        autoHeight: false,
+        autoplay: true,
+        responsive: {
+            992: {
+                items: 3,
+            },
+            768: {
+                items: 2,
+            },
+            0: {
+                items: 1,
+            },
+        }
+    });
+
+    /*--------------------------
+        Videos Slider
+    ----------------------------*/
+    $(".videos__slider").owlCarousel({
+        loop: true,
+        margin: 0,
+        items: 4,
+        dots: false,
+        nav: true,
+        navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
+        smartSpeed: 1200,
+        autoHeight: false,
+        autoplay: true,
+        responsive: {
+            992: {
+                items: 4,
+            },
+            768: {
+                items: 3,
+            },
+            576: {
+                items: 2,
+            },
+            0: {
+                items: 1,
+            }
+        }
+    });
+
+    /*------------------
+		Magnific
+	--------------------*/
+    $('.video-popup').magnificPopup({
+        type: 'iframe'
+    });
+
+})(jQuery);
